858baba12f144c3f5be626d35758341d5ee767af
Shiva-mkr <Shiva.pushparajp@gmail.com>
2017-04-20 23:44:30 +0530

    Revert "Revert "Connect N Solution Arch""
    This reverts commit f8d33d253c0dadf7d9604965606ec447f6055c12.

---
+++ ConnectNAPI.Utility/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectNAPI.Utility
+{
+    public class Constants
+    {
+        public const string Patient_details_fileName = "PatientInformation.txt";
+        public const string Vital_Update_fileName = "VitalUpdate.txt";
+    }
+    
+}
